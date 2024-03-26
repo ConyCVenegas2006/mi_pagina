783872eda998361c838a352643e82623b22d5a64
ConyCVenegas2006 <162452029+ConyCVenegas2006@users.noreply.github.com>
2024-03-26 09:40:20 -0300

    Add files via upload

---
+++ x.js
@@ -0,0 +1,97 @@
+//Declarando STRING
+
+let producto = " Lacteos"
+let nombre = "Pedro"
+let apellido = "Urrutia"
+let Empresa = "soprole" 
+
+//Declarando NUMBER
+
+let año = 1850
+let caducidad = 260424
+let edad = 26
+let altura = 1.80
+
+//Declarando Booleanos
+let deCalidad = true 
+let extranjera = false 
+let inicioSeción = true
+let desconectado = false 
+
+
+let Arreglo = [ "soprole", "Lacteos",  "Pedro" , " Urrutia" , 1850 , 260424 , 26 , 1.80 ,  true , false , true , false]
+let objeto = {Empresa : "soprole" , producto : "lacteos" , nombre : "Pedro" , apellido : "Urrutia" ,  año : 1850 , caducidad : 260424 , edad : 26 , altura : 1.80 , deCalidad : true , extranjera : false , iniciopSeción : true , desconectado : false}
+
+console.log("Empresa")
+console.log(Empresa)
+console.log(producto)
+console.log(nombre)
+console.log(apellido)
+console.log(año)
+console.log(caducidad)
+console.log(edad)
+console.log(altura)
+console.log(deCalidad)
+console.log(extranjera)
+console.log(inicioSeción)
+console.log(desconectado)
+console.log(Arreglo)
+console.log(objeto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
